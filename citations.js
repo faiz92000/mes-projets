216eb5f8049d3fe3e4c54a873024458ff8020bb2
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2023-04-06 19:33:01 +0200

    Update citations.js

--- citations.js
+++ citations.js
@@ -11,13 +11,13 @@
 	<script>
 	
 		let citations = [
-			"La vie est un mystère qu'il faut vivre, et non un problème à résoudre. - Gandhi",
-			"Il n'y a qu'une façon d'échouer, c'est d'abandonner avant d'avoir réussi. - Georges Clemenceau",
-			"La patience est amère, mais son fruit est doux. - Jean-Jacques Rousseau",
-			"Le bonheur est souvent la seule chose qu'on puisse donner sans l'avoir et c'est en le donnant qu'on l'acquiert. - Voltaire",
-			"Tout ce que nous avons à décider, c'est ce que nous devons faire du temps qui nous est imparti. - J.R.R. Tolkien",
-      			"Je suis le developpeur qu'il vous faut pour votre projet. - Faïz Ali",
-      			"N'importe quel idiot peut écrire du code qu'un ordinateur peut comprendre. Les bons programmeurs écrivent du code que les humains peuvent comprendre. - Martin Fowler"
+			"La passion et les rêves sont comme le temps, rien ne peut les arrêter, et il en sera ainsi tant qu’il aura des hommes prêts à donner un sens au mot « LIBERTE » – Gold D.Roger",
+			" Un pirate doit être fier de son nom. Tâchez de vous souvenir du mien – Monkey D.Luffy",
+			"À quoi te sert ton ambition si tu ne peux pas protéger ton capitaine ? – Roronoa Zoro",
+			"Quoi qu’il arrive, ne perdez jamais espoir face aux adversités du monde qui vous entoure ! Soyez assez forts pour pouvoir rire de tout, ne vous préoccupez pas du regard des autres. – Belmer",
+			"J’ai beaucoup changé. Avant, pour avoir l’air plus fort, j’inventais des mensonges. Maintenant, je n’ai plus besoin de mentir. – Usopp",
+      		"Ne pas voir la pourriture de ce monde, est un plaisir uniquement connu des aveugles. – Fujitora",
+      		"Il y a des batailles qu’on ne gagne pas avec les poings ! – Barbe noire"
 		];
 
 		
@@ -27,5 +27,14 @@
 			citation.innerHTML = citations[index];
 		}
 	</script>
+	<style>
+        
+		body {
+  background-image: url("Logo_One_piece.svg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+	</style>
 </body>
 </html>
